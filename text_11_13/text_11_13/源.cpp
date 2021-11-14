bbd14eefadd1512a473c3c780375350dc04bfce3
PeterYuCoder <1961007606@qq.com>
2021-11-14 09:36:19 +0800

    循环

---
+++ text_11_13/text_11_13/源.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (((i % 4 == 0) && (i % 100 != 0))||(i % 400 == 0))
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+#include<math.h>
+int main()
+{
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j <= sqrt(i); j + 2)
+		{
+			if (i%j==0)
+			{
+				break;
+			}
+			else{
+				printf("%d", i);
+			}
+		}
+
+	}
+	return 0;
+}
